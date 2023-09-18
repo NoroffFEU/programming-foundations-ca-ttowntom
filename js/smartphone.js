1a9d4e36f405e65da83ffb12d38a6f8abb7418ea
ttowntom <117744552+ttowntom@users.noreply.github.com>
2023-09-18 16:13:12 +0200

    added smartphone.js and object

---
+++ js/smartphone.js
@@ -0,0 +1,48 @@
+const smartphone = {
+	brand: {
+		name: "Google",
+		model: "Pixel 7",
+	},
+	os: {
+		name: "Andriod",
+		version: 13,
+	},
+	spesifications: {
+		color: "Obisidian",
+		cpu: "Google Tensor G2 Titan M2",
+		storageCapacityGB: 128,
+		ramGB: 8,
+		battery: {
+			batteryLifeHours: 24,
+			capacitymAh: 4355,
+			fastCharging: true,
+			wirelessCharging: true,
+		},
+		screen: {
+			sizeIn: 6.3,
+			resolution: "1080 x 2400 pixels",
+			maxFrequency: 90,
+			bitDepth: 24,
+			maxNit: 1400,
+		},
+		cameras: [
+			{
+				position: "rear",
+				megapixels: 50,
+				viewAngle: 82,
+				apertureF: 1.85,
+			},
+			{
+				position: "front",
+				megapixels: 10.8,
+				viewAngle: 92.8,
+				apertureF: 2.2,
+			},
+		],
+	},
+	alertLowBattery: function () {
+		if (phone.battery.capacitymAh < 500) {
+			console.log("Low battery!");
+		}
+	},
+};
